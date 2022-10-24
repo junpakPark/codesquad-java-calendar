0419bc6b9bcd4cfc9fe17d881ece19c80d88cab1
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-24 14:24:39 +0900

    add isLeapYear function

--- src/junpak/calendar/Prompt.java
+++ src/junpak/calendar/Prompt.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private final static String PROMPT = "cal> ";
-
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		while (true) {
+			System.out.println("연도를 입력하세요.");
+			System.out.print("Year> ");
+			int year = scanner.nextInt();
 			System.out.println("달을 입력하세요.");
-			System.out.print(PROMPT);
+			System.out.print("Month> ");
 			int month = scanner.nextInt();
 			if (month == -1) {
 				break;
@@ -21,7 +22,7 @@ public class Prompt {
 				System.out.println("입력값을 확인해주세요.");
 				continue;
 			}
-			cal.printCalendar(2022, month);
+			cal.printCalendar(year, month);
 		}
 
 		System.out.println("Bye~");
