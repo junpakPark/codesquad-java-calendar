1a079b102fab2e505f54dbd9f4e8493fb405d7e5
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-24 10:54:37 +0900

    Update using class method

--- src/junpak/calendar/Calendar.java
+++ src/junpak/calendar/Calendar.java
@@ -3,13 +3,22 @@ package junpak.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String[] args) {
+
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+
+	public static void main(String[] args) {
 
 		//나의 풀이
 		int[] monthLength = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -24,10 +33,11 @@ public class Calendar {
 
 		//강의 설명
 		Scanner scanner1 = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
 		int month1 = scanner.nextInt();
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d월은 %d 일까지 있습니다. \n", month1, maxDays[month1 - 1]);
+		System.out.printf("%d월은 %d 일까지 있습니다. \n", month1, cal.getMaxDaysOfMonth(month1));
+		cal.printSampleCalendar();
 		scanner1.close();
 	}
 }
